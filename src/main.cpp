41bdf63b6dbe6c14c86a7afcb826bbe07a00af63
bugger2 <usernamesarenoteasy@proton.me>
2024-01-05 23:49:01 -0700

    simple steps to show proof of concept doesnt hang or have a memory leak or whatnot

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include "KalmanFilter.hpp"
 
 int main(void) {
+    InputVector u;
+    u.setZero();
+
+    OutputVector y;
+    y.setZero();
+    
     KalmanFilter filter;
+
+    filter.predict(u);
+    filter.update(y, u);
 
     return 0;
 }
